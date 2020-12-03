d95c38842f3ee9dfc1058cec9b48b362529c3009
Omerdan <Omerdan03@gmail.com>
2020-12-03 16:31:25 +0200

    new_sql_table tamplate

---
+++ new_sql_table.sql
@@ -0,0 +1,7 @@
+-- column types float, int, datetime, varchar (255), bigint ....
+
+CREATE TABLE `table_name` (
+  `column1` column1_type,
+  `column2` column1_type,
+  PRIMARY KEY (column1, column2)
+);
